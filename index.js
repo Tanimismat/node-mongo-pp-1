3c23d7b8def4e496ee6930245a8be122470c2d69
Tanim Ismat <tanim.ismat@gmail.com>
2022-08-27 13:22:23 +0600

    root route created

--- index.js
+++ index.js
@@ -4,8 +4,22 @@ const colors = require("colors");
 // console.log(http);
 
 const server = http.createServer(function (req, res) {
-	res.writeHead(200, { "content-Type": "text/plain" });
-	res.end("hello");
+	console.log(req.url);
+	let path = req.url;
+	switch (path) {
+		case "/":
+			res.writeHead(200, { "Content-Type": "text/html" });
+			res.write(`<p>Welcome to Full Stack Development.</p>`);
+			res.end();
+			break;
+		default:
+			res.writeHead(200, { "Content-Type": "text/html" });
+			res.write(`<p>Page not found</p>`);
+			res.end();
+	}
+	// res.writeHead(200, { "Content-Type": "text/html" });
+	// res.write(`<This is home page/>`);
+	// res.end();
 });
 server.listen(5000);
-console.log(colors.green("Listening to the port"));
+console.log(colors.blue("Listening to the port"));
